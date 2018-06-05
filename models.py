dedeacc8e2cd04eaea5624fa811c279f0995dcc7
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2018-06-05 11:54:28 +0200

    start of skeleton of recurrent attention model

--- models.py
+++ models.py
@@ -77,9 +77,97 @@ def cnn_model_fn(features, labels, mode, params):
     return
 
 
+################################################################
+# Recurrent Attention Model
+# From Mnih et al 2014, Recurrent Models of Visual Attention
+################################################################
+
+
+class Retina(object):
+
+    def __init__(self, img_size, patch_size, num_patches=1):
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+    def __call__(self, images, locs):
+        # TODO: support multiple patches
+        return tf.image.extract_glimpse(
+            images,
+            [self.patch_size, self.patch_size],
+            locs)
+
+
+class GlimpseNetwork(object):
+
+    def __init__(self, img_size, patch_size, g_size, l_size, out_size):
+        self.retina = Retina(img_size, patch_size)
+        self.g_size = g_size
+        self.l_size = l_size
+        self.out_size = out_size
+
+    def __call__(self, images, locs):
+
+        # -- patches: [batch_size, patch_size, patch_size, 3]
+        patches = self.retina(images, locs)
+
+        # TODO: Lewis implements Glimpse Network here
+
+        return tf.Variable(tf.zeros([tf.shape(images)[0], self.out_size]),
+                           name="dummy_glimpse")
+
+
+class LocationNetwork(object):
+
+    def __init__(self, loc_dim, std=0.2, sampling=False):
+        self.loc_dim = loc_dim
+        self.std = std
+        self.sampling = sampling
+
+    def __call__(self, rnn_output):
+
+        # TODO: Lewis implements Location Network here
+        # tanh to force [-1, 1] values
+        means = tf.tanh(
+            tf.Variable(tf.zeros([tf.shape(rnn_output)[0], self.loc_dim]),
+                        name="dummy_loc_mean"))
+
+        if self.sampling:
+            # tanh to force [-1, 1] values
+            locs = tf.tanh(
+                means + tf.random_normal(
+                    [tf.shape(rnn_output)[0], self.loc_dim],
+                    std=self.std))
+        else:
+            locs = means
+
+        return locs, means
+
+
+class GlimpseDecoder(tf.contrib.seq2seq.Decoder):
+
+    def __init__(self, glimpse_network, location_network, rnn_cell, params):
+        return
+
+    def initialize(self):
+        return
+
+    def step(self, time, inputs, state):
+        return
+
+
 def ram_model_fn(features, labels, mode, params):
 
     images = features[params['img_feature_name']]
     net = images
+
+    with tf.variable_scope('glimpse_network'):
+        glimpse_net = GlimpseNetwork(params['img_size'], params['patch_size'],
+                                     params['g_size'], params['l_size'],
+                                     params['glimpse_out_size'])
+
+    is_training = mode == tf.estimator.ModeKeys.TRAIN
+    with tf.variable_scope('location_network'):
+        location_net = LocationNetwork(params['loc_dim'], sampling=is_training)
 
     return
